c90cc2aad25c0de10790ce40182a214870c252dc
sachelaride42 <mateus.sachelaride@gmail.com>
2024-09-17 15:39:14 -0400

    Criação de projeto Flutter

---
+++ agenda_de_contatos/README.md
@@ -0,0 +1,3 @@
+# agenda_de_contatos
+
+A new Flutter project.
---
+++ agenda_de_contatos/android/app/src/main/kotlin/com/example/agenda_de_contatos/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.agenda_de_contatos
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
